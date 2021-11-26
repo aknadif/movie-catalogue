93d5c737b59eb0c6cf132805ff6584005237a5ea
Nadhif <aknadif58@gmail.com>
2021-11-26 20:50:41 +0700

    Adding Service Worker

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: "d8879566c63eb5f02808a0c4efb09641",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
